14a04d0caa98579779970cab8b003d1503dbd357
Samiulation <98295059+Samiulation@users.noreply.github.com>
2022-03-04 17:39:25 +0600

    Add files via upload

--- galt_script.js
+++ galt_script.js
@@ -2,7 +2,7 @@ var this_pc_logo = "./img/1017.png";
 var power_btn = "./img/108.png";
 var recycle_bin = "./img/500.png";
 var ecplayer = "./img/ec%20play.png";
-var windows_wallpaper_default = "./img/win10.png";
+var windows_wallpaper_default = "./img/win10.jpg";
 var win32 = "./img/win32.png";
 var settings = "./img/133.png";
 var winphotos = "./img/Photos.png";
@@ -14,10 +14,11 @@ var wincalc = "./img/Calc.png";
 var winmaps = "./img/Maps.png";
 var lvgo = "./img/lv.png";
 var people = "./img/People.png";
-var raid = "https:/\/\images-platform.99static.com/gDUuSQxcfNphPc9wud4OqWbMapA=/0x0:1500x1500/500x500/top/smart/99designs-contests-attachments/109/109282/attachment_109282547";
+var raid = "./img/games/raid_logo.png";
 var ttl = document.getElementById('pag_details').getAttribute('var');
 if(ttl == "desktop_32"){
 function setwall(){
+if(!localStorage.fluidlv){
 if(!localStorage.vdolv){
     if(!localStorage.pdolv){
     document.getElementById('wallpaper').innerHTML = "";
@@ -30,6 +31,12 @@ document.getElementById('input_of_localw').value = localStorage.pdolv;
 else{document.getElementById('wallpaper').style="width: 100%;height: 100%;position: fixed;top: 0;left: 0;right: 0;bottom: 0;background-color: black;";document.getElementById('wallpaper').innerHTML = "<video autoplay id='walvid' muted loop src='"+localStorage.vdolv+"' style='object-fit: fill;width:100%;height:100%;position:fixed;right: 0;top:0;' ></video><div style='position:fixed;top:0;left:0;width:100%;height:100%;'>";}
 document.getElementById('input_of_localv').value = localStorage.vdolv;
 document.getElementById('input_of_localw').value = localStorage.pdolv;
+}
+else{
+    document.getElementById('input_of_localv').value = "";
+    document.getElementById('input_of_localw').value = "";
+document.getElementById("fluid").value = localStorage.fluidlv;
+document.getElementById('wallpaper').innerHTML = "<iframe src='img/wallpapers/fluid.html' style='width:100%;height:100%;position:fixed;top:0;left:0;border:none;'>";}
 }setwall();
 document.getElementById('clm_1').style = "background-color:transparent;background-image: url("+this_pc_logo+");background-size: 91% 91%;background-repeat:no-repeat;border: none;position: fixed;width: 81px;height: 81px;top: 6px;"
 document.getElementById('clm_2').style = "background-color:transparent;background-image: url("+recycle_bin+");background-size: 91% 91%;background-repeat:no-repeat;border: none;position: fixed;width: 81px;height: 81px;top: 102px;"
